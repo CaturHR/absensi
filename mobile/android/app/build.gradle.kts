e40068b603bef818c7fe7a0fc29f965930fce752
Royyyhan <yantov3ah@gmail.com>
2026-09-24 12:59:20 +0700

    rr

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -25,9 +25,6 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        if (minSdk < 21) {
-            minSdk = 21
-        }
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
